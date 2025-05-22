e6dbb960749cd4d227c492f77d90a3c38d27af83
Jay S <js4cap@gmail.com>
2025-05-22 12:42:44 -0500

    Carousel fix, placeholders there

--- budgeting/src/Pages/Home.jsx
+++ budgeting/src/Pages/Home.jsx
@@ -4,17 +4,29 @@ const Home = () => {
     return (
         <div className="home-page">
             <h1>Budgeting app (Change this name) </h1>
-            <h2>An introduction</h2>
+            <h2>Read our user's stories!</h2>
             <div className="home-page-photo-carousel">
                 <div className="group">
-                    <div className="card">A</div>
-                    <div className="card">B</div>
-                    <div className="card">C</div>
+                    <div className="card">
+                        <p>I used to be a loser, until I wasn't!</p>
+                        <h3>-That guy</h3>
+                    </div>
+                    <div className="card">
+                        <p>That guy really was a loser lol</p>
+                        <h3>-Hater</h3>
+                    </div>
+                    <div className="card"><img src="https://static.wikia.nocookie.net/characterprofile/images/4/4e/RaidenMKXrender.png" alt="raiden"></img>Pikachue kamehameha</div>
                 </div>
-                <div aria-hidden="true" className="group">  {/*TODO: aria-hidden not functioning properly */}
-                    <div className="card">A</div>
-                    <div className="card">B</div>
-                    <div className="card">C</div>
+                <div aria-hidden="true" className="group">
+                    <div className="card">
+                        <p>I used to be a loser, until I wasn't!</p>
+                        <h3>-That guy</h3>
+                    </div>
+                    <div className="card">
+                        <p>That guy really was a loser lol</p>
+                        <h3>-Hater</h3>
+                    </div>
+                    <div className="card"><img src="https://static.wikia.nocookie.net/characterprofile/images/4/4e/RaidenMKXrender.png" alt="raiden"></img>Pikachue kamehameha</div>
                 </div>
             </div>
         </div>
