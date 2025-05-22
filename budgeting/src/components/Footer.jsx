5a7bdaff6366c1f6784ad36645b1084f2da12df1
EastAsianInc <js4cap@gmail.com>
2025-05-22 09:16:25 -0500

    Fixed external links to open in new tab

--- budgeting/src/components/Footer.jsx
+++ budgeting/src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router";
+
 const Footer = () => (
     <footer className="footer" >
         <p>
             Follow me on:
-            <a href="https://www.linkedin.com/in/jay-sakabu-b78a04136/">LinkedIn</a>
-            <a href="https://github.com/Jay-Sakabu" > GitHub</a>
+            <Link to="https://www.linkedin.com/in/jay-sakabu-b78a04136/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
+            <Link to="https://github.com/Jay-Sakabu" target="_blank" rel="noopener noreferrer"> GitHub</Link>
         </p>
     </footer>
 );
