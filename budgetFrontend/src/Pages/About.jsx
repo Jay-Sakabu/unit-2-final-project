7f07bda80437e87b62c1d4bf6f9b011923337cc4
Jay S <js4cap@gmail.com>
2025-07-16 09:19:51 -0500

    Copied Frontend

---
+++ budgetFrontend/src/Pages/About.jsx
@@ -0,0 +1,19 @@
+const About = () => {
+    return (
+        <div className="about-page">
+            <h1>
+                Welcome to the first step to financial freedom!
+            </h1>
+            <div className="about-page-text">
+                <p>Managing your hard-earned money can feel overwhelming, but it doesn't have to be!</p>
+                <br></br>
+                <p>This app was built to take the stress out of budgeting by offering a simple, no-frills approach that puts your needs first. Whether you're just starting out or looking to get a better handle on your finances, you'll find that all it takes is a few quick inputs to begin building a personalized, principled budget.</p>
+                <br></br>
+                <p>No jargon, no judgment, no hassle. Just the essentials to help you take control, stay organized, and start making your money work for you!</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default About;
