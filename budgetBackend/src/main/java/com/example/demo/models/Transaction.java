f4fa9f880a1e6775cbeb9e423ddff0ca13ba92c2
Jay S <js4cap@gmail.com>
2025-08-01 18:05:50 -0500

    Transactions updated, User class added, FK relation correctly generates

--- budgetBackend/src/main/java/com/example/demo/models/Transaction.java
+++ budgetBackend/src/main/java/com/example/demo/models/Transaction.java
@@ -1,17 +1,74 @@
 package com.example.demo.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-//
+import java.time.LocalDate;
 
+//Transaction model representing a financial transaction for a user
+// It includes fields for user, category, date, amount, and description
 @Entity
+@Table(name = "transactions")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_transaction_user"))
+    private User user;
+    private String category; // "Needs", "Wants", "Savings"
+    private LocalDate date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private double amount;
+    private String description;
 
 
 }
