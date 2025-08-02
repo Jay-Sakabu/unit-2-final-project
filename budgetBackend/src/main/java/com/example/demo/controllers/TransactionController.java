7563f6222980778e28573be25af3cd728625e28c
Jay S <js4cap@gmail.com>
2025-08-01 19:08:24 -0500

    Controllers added, Transaction Controller naive implemented

--- budgetBackend/src/main/java/com/example/demo/controllers/TransactionController.java
+++ budgetBackend/src/main/java/com/example/demo/controllers/TransactionController.java
@@ -1,4 +1,38 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Transaction;
+import com.example.demo.repositories.TransactionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+    private final TransactionRepository transactionRepository;
+
+    public TransactionController(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    @GetMapping("/transactions")
+    public List<Transaction> all() {
+        return transactionRepository.findAll();
+    }
+
+    @PostMapping("/transactions")
+    public Transaction addTransaction(@RequestBody Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @PutMapping("/transactions/{id}")
+    public Transaction updateTransaction(@PathVariable Integer id, @RequestBody Transaction transaction) {
+        transaction.setId(id);
+        return transactionRepository.save(transaction);
+    }
+
+    @DeleteMapping("/transactions/{id}")
+    public void deleteTransaction(@PathVariable Integer id) {
+        transactionRepository.deleteById(id);
+    }
 }
