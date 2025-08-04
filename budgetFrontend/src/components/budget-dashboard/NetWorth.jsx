a0533ddfbbc748bbe236edafdf1fc3365eb4c236
Jay S <js4cap@gmail.com>
2025-08-04 14:43:09 -0500

    Resolved TODO's removed unused assets

--- budgetFrontend/src/components/budget-dashboard/NetWorth.jsx
+++ budgetFrontend/src/components/budget-dashboard/NetWorth.jsx
@@ -18,7 +18,6 @@ const NetWorth = () => {
     return (
         <div className="dashboard-box">
             <h2>Net Worth</h2>
-            {/* TODO: Create styles for these items */}
             {/* toLocaleStrings for proper currency displaying */}
             <p>Total Assets: {totalAssets.toLocaleString("en-US", {style:"currency", currency:"USD"})}</p>
             <p>Total Liabilities: {totalLiabilities.toLocaleString("en-US", {style:"currency", currency:"USD"})}</p>
